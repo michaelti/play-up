929079ac03bbf85bdf283a5aede82d9dd7e94eb6
Sammy Abukmeil <sammyabukmeil@gmail.com>
2024-06-12 12:56:15 +0100

    Add HTTP hooks and add game endpoint

---
+++ client/src/hooks/useAxios.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function useAxios(url, deps = []) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_BACKEND_URL}${url}`
+      );
+      setData(response.data);
+    } catch (error) {
+      setError(error);
+    }
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, deps);
+
+  return [data, loading, error];
+}
