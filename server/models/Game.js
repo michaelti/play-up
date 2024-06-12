7e28fa9431600c3ebb358bc532879983266f26d2
Sammy Abukmeil <sammyabukmeil@gmail.com>
2024-06-12 12:12:55 +0100

    Integrate with JSON files

--- server/models/Game.js
+++ server/models/Game.js
@@ -1,10 +1,8 @@
-const retrieveAllGames = () => {
-  const mockJson = [
-    { id: "f1babed6-c59f-40c3-9827-29dff355b8d0", name: "Game 1" },
-    { id: "9690accb-de10-41bb-9a2b-d1e9ce07ee13", name: "Game 2" },
-  ];
+import fs from "fs";
 
-  return mockJson;
+const retrieveAllGames = () => {
+  const gamesJson = fs.readFileSync("./data/games.json");
+  return JSON.parse(gamesJson);
 };
 
 const retrieveSingleGame = (id) => {
