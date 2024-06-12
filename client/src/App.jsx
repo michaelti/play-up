b7d1df227b7d91014e1079281730b6526e89c97b
Michael <mdismatsek@brainstation.io>
2024-06-12 14:21:34 +0100

    Add pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Library from "./pages/Library/Library";
+import Recent from "./pages/Recent/Recent";
+import Rankings from "./pages/Rankings/Rankings";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Library />} />
+        <Route path="/rankings" element={<Rankings />} />
+        <Route path="/recent" element={<Recent />} />
       </Routes>
     </BrowserRouter>
   );
