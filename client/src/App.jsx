837e693f3db9a8dabb38cf34a4feb3e68e258485
Sammy Abukmeil <sammyabukmeil@gmail.com>
2024-06-12 15:31:06 +0100

    WIP: Work on new match page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Library from "./pages/Library/Library";
 import Recent from "./pages/Recent/Recent";
 import Rankings from "./pages/Rankings/Rankings";
+import NewMatch from "./pages/NewMatch/NewMatch";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Library />} />
         <Route path="/rankings" element={<Rankings />} />
         <Route path="/recent" element={<Recent />} />
+        <Route path="/new-match/:gameId" element={<NewMatch />} />
       </Routes>
     </BrowserRouter>
   );
